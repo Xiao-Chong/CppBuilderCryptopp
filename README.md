a631740c18dec4fe4540f14fac0b2035f5ddd425
Sam Shaw <qs.xiao@gmail.com>
2014-09-21 05:13:17 +0800

    Delete some unnecessary comments. Add more description in README.md

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-CppBuilderCryptopp
+
 ==================
 
-Crypto++ for C++ builder (mainly for version XE7)
+Crypto++ version 5.6.2 for C++ builder (Tested for version XE7)
+
+Android, Win32, Win64 were tested OK.
+IOS does not test yet.
+
+Pleases Note that original cryptest_bds project is not working in
+XE7, however, in XE is OK.  Although this project may not compiled,
+it is not necessary for building lib file.
--- Unit1.cpp
+++ Unit1.cpp
@@ -23,32 +23,18 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Button1Click(TObject *Sender)
 {
-//Key and IV setup
+		//Key and IV setup
     //AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-
     //bit). This key is secretly exchanged between two parties before communication
-    //begins. DEFAULT_KEYLENGTH= 16 bytes
+		//begins. DEFAULT_KEYLENGTH= 16 bytes
 		byte key[ CryptoPP::AES::DEFAULT_KEYLENGTH ], iv[ CryptoPP::AES::BLOCKSIZE ];
-		//memset( key, 0x00, CryptoPP::AES::DEFAULT_KEYLENGTH );
-   // memset( iv, 0x00, CryptoPP::AES::BLOCKSIZE );
 
-    //
-    // String and Sink setup
-    //
-		std::string plaintext = "Sam is Geek, andow is the time for all good men to come to the aide...";
+		std::string plaintext = "Welcome to C++ Builder Delphi Taiwan G+ Community\nhttps://plus.google.com/communities/118037201839816898111";
 		std::string ciphertext;
 		std::string decryptedtext;
 
 		this->Memo1->Lines->Add(plaintext.c_str());
-    //
-    // Dump Plain Text
-    //
-//    std::cout << "Plain Text (" << plaintext.size() << " bytes)" << std::endl;
-//		std::cout << plaintext;
-//		std::cout << std::endl << std::endl;
 
-    //
-    // Create Cipher Text
-    //
 		CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
 		CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption( aesEncryption, iv );
 
@@ -65,26 +51,11 @@ void __fastcall TForm1::Button1Click(TObject *Sender)
 		//db[db.High] = 0;
 		db2[db2.High] = 0;
 
-		this->Memo1->Lines->Add(ciphertext.c_str());
+		//this->Memo1->Lines->Add(ciphertext.c_str());
 
 		this->Memo1->Lines->Add("");
 		this->Memo1->Lines->Add(TEncoding::ASCII->GetString(db2));
 		this->Memo1->Lines->Add("");
-    //
-    // Dump Cipher Text
-		//
-//    std::cout << "Cipher Text (" << ciphertext.size() << " bytes)" << std::endl;
-//
-//		for( int i = 0; i < ciphertext.size(); i++ ) {
-//
-//				std::cout << "0x" << std::hex << (0xFF & static_cast<byte>(ciphertext[i])) << " ";
-//		}
-//
-//		std::cout << std::endl << std::endl;
-
-    //
-    // Decrypt
-		//
 
 		CryptoPP::AES::Decryption aesDecryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
 		CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption( aesDecryption, iv );
@@ -92,13 +63,6 @@ void __fastcall TForm1::Button1Click(TObject *Sender)
 		CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink( decryptedtext ) );
 		stfDecryptor.Put( reinterpret_cast<const unsigned char*>( ciphertext.c_str() ), ciphertext.size() );
 		stfDecryptor.MessageEnd();
-
-    //
-    // Dump Decrypted Text
-    //
-//    std::cout << "Decrypted Text: " << std::endl;
-//		std::cout << decryptedtext;
-//		std::cout << std::endl << std::endl;
 
 		this->Memo1->Lines->Add(decryptedtext.c_str());
 }
